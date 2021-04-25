8bcfc0781362182eb34e2eed27494930fbf15f81
Issei <chatani171@gmail.com>
2021-04-25 13:43:01 +0900

    rails課題7を回答

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "名前の表示" do
+    # パラメータをまとめて定義
+    let(:params) { { name: "satou", age: 1 } }
+    let(:params2) { { name: "satou", age: 15 } }
+    let(:params3) { { name: "satou", age: 30 } }
+  # pending "add some examples to (or delete) #{__FILE__}"
+    it "年齢1：ユーザの名前が取得できること" do
+      user = User.new(params)
+      expect(user.disp_name).to eq "satouちゃん"
+    end
+    it "年齢15：ユーザーの名前が取得できること" do
+      user = User.new(params2)
+      expect(user.disp_name).to eq "satou君"
+    end
+    it "年齢20：ユーザーの名前が取得できること" do
+      user = User.new(params3)
+      expect(user.disp_name).to eq "satouさん"
+    end
+  end
+end
